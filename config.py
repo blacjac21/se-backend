5f738e88b86dce72b9e87f5e4e6bad0949b3dd45
blacjac21 <blacjac21@gmail.com>
2022-11-14 11:41:07 +0530

    ss

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     DEBUG = True
-    SQLITE_DB_DIR = os.path.join(basedir,"../db")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join()
+    SQLITE_DB_DIR = os.path.join(basedir,"../DB")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(SQLITE_DB_DIR,"clubs.sqlite3")
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    
